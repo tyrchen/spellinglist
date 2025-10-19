480d323e144c1e8958b5b011c5800cccbc490b2d
Cynthia <c@test.com>
2025-10-18 21:04:49 -0700

    Initial Commit

---
+++ spellinglist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  spellinglist
+//
+//  Created by cynthia on 10/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ spellinglist/spellinglistApp.swift
@@ -0,0 +1,17 @@
+//
+//  spellinglistApp.swift
+//  spellinglist
+//
+//  Created by cynthia on 10/18/25.
+//
+
+import SwiftUI
+
+@main
+struct spellinglistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
